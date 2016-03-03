3d664e3ca49e6c82a830546d0da6596072237089
Tomislav Tenodi <tomislav.tenodi@gmail.com>
2016-03-03 18:46:44 +0100

    Add TODO for strict options in mongoose Schema

--- models/thing.js
+++ models/thing.js
@@ -2,6 +2,9 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+// TODO: Tenodi - Research if we should add strict: 'throw' option to produce
+// errors when values passed to model constructor are not specified in schema.
+// http://mongoosejs.com/docs/guide.html#strict
 const ThingSchema = new Schema({
   name: String,
   info: String,
