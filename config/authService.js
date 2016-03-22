cd9e06da40d8e74616ad7362921f8519b09d8941
Tomislav Tenodi <tomislav.tenodi@gmail.com>
2016-03-22 13:47:09 +0100

    Add comments for authService config

--- config/authService.js
+++ config/authService.js
@@ -1,3 +1,6 @@
+// Configuration for Shoutem authentication service.
+//    - url: endpoint for service
+//    - sub: subject for authentication service contained in JWT
 export default {
   url: 'http://auth.shoutem.com',
   sub: 'service:authentication',
