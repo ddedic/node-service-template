bd9edd4601252982f9a36719535a86534e4b0a36
Tomislav Tenodi <tomislav.tenodi@gmail.com>
2016-03-22 18:22:55 +0100

    Add balerion.sauros as test auth service

--- config/authService.js
+++ config/authService.js
@@ -1,7 +1,10 @@
 // Configuration for Shoutem authentication service.
 //    - url: endpoint for service
 //    - sub: subject for authentication service contained in JWT
+// TODO: Tenodi - Change this to api.shoutem.com once it's deployed
+// TODO: Tenodi - These informations shouldn't be held in configuration files
+//                but in environment variables
 export default {
-  url: 'http://auth.shoutem.com',
+  url: 'https://api.balerion.sauros.hr',
   sub: 'service:authentication',
 };
