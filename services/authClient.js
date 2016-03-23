efac2281f0914b22d55a872dc3ec4fd6cfcb170f
Tomislav Tenodi <tomislav.tenodi@gmail.com>
2016-03-23 20:19:57 +0100

    Fix checking payload and authentication errors on client

--- services/authClient.js
+++ services/authClient.js
@@ -2,6 +2,7 @@ import debug from 'debug';
 import jwt from 'jsonwebtoken';
 import request from 'request';
 import jwtConfig from '../config/jwt';
+import _ from 'lodash';
 
 const log = debug('skeleton:auth');
 
@@ -12,6 +13,10 @@ export default class {
 
   verifyToken(payload) {
     log('Auth client is verifying token.');
+    if (!(_.isObject(payload)) || _.isEmpty(payload)) {
+      // TODO: Tenodi - implement user-defined errors and error handlers
+      return Promise.reject(new Error('Payload must be non-empty object'));
+    }
 
     // Generate token
     const token = jwt.sign(payload, jwtConfig.secret, { algorithm: jwt.algorithm });
@@ -26,15 +31,16 @@ export default class {
 
     return new Promise((resolve, reject) => {
       request({
-        method: 'PUT',
+        method: 'POST',
         uri: `${this.authServiceUrl}/v1/validate-auth-token`,
         json: true,
         body: requestBody,
-      }, (error) => {
-        if (error) {
-          reject(error);
-        } else {
+      }, (error, response) => {
+        if (!error && response.statusCode === 200) {
           resolve(payload);
+        } else {
+          // TODO: Tenodi - implement user-defined errors and error handlers
+          reject(new Error('Error while validating token'));
         }
       });
     });
