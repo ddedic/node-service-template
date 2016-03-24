1be36293837f6b3318800898bb1633bcdb08eb8a
Tomislav Tenodi <tomislav.tenodi@gmail.com>
2016-03-24 10:49:04 +0100

    Change api to correct for shoutem authentication service

--- config/auth.js
+++ config/auth.js
@@ -3,13 +3,12 @@
 //      - url: endpoint for service
 //      - sub: subject for authentication service contained in JWT
 //    - rlm: authentication realm where the token is valid
-// TODO: Tenodi - Change this to api.shoutem.com once it's deployed
 // TODO: Tenodi - Some of these informations which are variable shouldn't
 //                hardcoded here, but rather to take from environment
 // TODO: Tenodi - Implement which subject types are valid in JWT token.
 export default {
   authService: {
-    url: 'https://api.balerion.sauros.hr',
+    url: 'https://api.shoutem.com',
     sub: 'service:authentication',
   },
   jwt: {
