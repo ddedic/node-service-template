b4861febe1bd934b13ef90a772574689ce01ecf0
Tomislav Tenodi <tomislav.tenodi@gmail.com>
2016-03-25 14:36:11 +0100

    Finish tests for version validator class

--- test/services/versionValidator.js
+++ test/services/versionValidator.js
@@ -2,7 +2,9 @@ import * as chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 import Cache from '../../services/cache';
-import VersionValidator from '../../services/VersionValidator';
+import VersionValidator from '../../services/versionValidator';
+import RemoteApiValidator from '../../services/remoteApiValidator';
+import cacheConfig from '../../config/cache';
 
 chai.use(chaiAsPromised);
 const assert = chai.assert;
@@ -16,13 +18,27 @@ describe('Version Validator', function () {
     },
   };
 
+  // These payloads are used to distinguish payload which will cause
+  // authentication service to return error and other which will not
+  const goodPayload = {
+    sub: 'user:1234',
+    vsi: {
+      typ: 'v',
+      v: 3,
+    },
+  };
+
+  const badPayload = Object.assign({}, goodPayload, {
+    sub: 'user:123',
+  });
+
   before(function () {
     // Stub Cache implementation
     sinon.stub(Cache.prototype, 'get', () => ({ v: 2 }));
   });
 
   after(function () {
-    sinon.restore();
+    Cache.prototype.get.restore();
   });
 
   it('should reject payload without version', function () {
@@ -43,5 +59,33 @@ describe('Version Validator', function () {
 
   it('should resolve equal version', function () {
     return assert.isFulfilled(VersionValidator.authenticate(mockedPayload));
+  });
+
+  describe('Calling default validator', function () {
+    afterEach(function () {
+      RemoteApiValidator.authenticate.restore();
+    });
+
+    it('should resolve when authentication service returns payload', function () {
+      sinon.stub(RemoteApiValidator, 'authenticate', (payload) => Promise.resolve(payload));
+      return assert.isFulfilled(VersionValidator.authenticate(goodPayload));
+    });
+
+    it('should cache vsi when authentication service returns payload', function (done) {
+      sinon.stub(RemoteApiValidator, 'authenticate', (payload) => Promise.resolve(payload));
+      const spy = sinon.spy(Cache.prototype, 'set');
+
+      VersionValidator
+        .authenticate(goodPayload)
+        .then(() => {
+          assert.isTrue(spy.calledWith(goodPayload.sub, goodPayload.vsi, cacheConfig.vsiTTL));
+          done();
+        });
+    });
+
+    it('should reject when authentication service returns error', function () {
+      sinon.stub(RemoteApiValidator, 'authenticate', () => Promise.reject(new Error()));
+      return assert.isRejected(VersionValidator.authenticate(badPayload));
+    });
   });
 });
